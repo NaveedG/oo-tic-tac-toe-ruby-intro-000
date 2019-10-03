1ff4e984b4081a0179d86f322557c158c6a34b2f
Naveed Ganjani <nganjani@protonmail.com>
2019-10-03 21:23:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,4 +105,27 @@ class TicTacToe
     return true if won? || full? || draw?
   end
 
+  def winner
+    if won?
+      WIN_COMBINATIONS.each do |combo|
+        win_index_1 = combo[0]
+        win_index_2 = combo[1]
+        win_index_3 = combo[2]
+
+        position_1 = @board[win_index_1]
+        position_2 = @board[win_index_2]
+        position_3 = @board[win_index_3]
+
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+          return "X"
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return "O"
+        end
+
+      end
+    else
+      return nil
+    end
+  end
+
 end
