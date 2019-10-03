0f1dbc6109459d20d95b17c884e6ed4e718e424c
Naveed Ganjani <nganjani@protonmail.com>
2019-10-03 20:56:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,5 +37,9 @@ class TicTacToe
     return true if !(@board[position] == " " || @board[position] == "" || @board[position] == nil)
   end
 
+  def valid_move?(position)
+    return true if position.between?(0, 8) && !position_taken?(@board, position)
+  end
+
 
 end
